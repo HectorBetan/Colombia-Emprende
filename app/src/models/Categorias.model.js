bcfcc980ed3fca1c7b99082d0d5e63d71c9f9566
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-25 17:24:49 -0500

    login/register button

---
+++ app/src/models/Categorias.model.js
@@ -0,0 +1,13 @@
+export const categorias = [ "Moda", 
+    "Artesanias", 
+    "Turismo", 
+    "Tecnología", 
+    "Comida", 
+    "Belleza", 
+    "Salud y Bienestar", 
+    "Niños y Bebes", 
+    "Deportes", 
+    "Hogar", 
+    "Servicios", 
+    "Otros" 
+];
