546069b06b690c2c5da21ca2216fd3115fcfe7ae
Hector Betancourt <hhbetancourt92@gmail.com>
2022-07-27 16:23:27 -0500

    carousel-mobile

--- app/src/components/HomeView.js
+++ app/src/components/HomeView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/Carousel.style.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Banner1 from "../assets/bannerHome/banner1.jpg";
 import Banner2 from "../assets/bannerHome/banner3.jpg";
@@ -8,21 +9,21 @@ import Banner3 from "../assets/bannerHome/banner4.jpg";
 function HomeView() {
     return (
       <div>
-        <Carousel fade controls={false}>
+        <Carousel fade controls={false} className="mb-2">
             <Carousel.Item interval={4000}>
-              <img className="d-block w-100" src={Banner1} alt="..." />
+              <img className="d-block w-100 img-carousel-center" src={Banner1} alt="..." />
             </Carousel.Item>
             <Carousel.Item interval={4000}>
-              <img className="d-block w-100" src={Banner2} alt="..." />
+              <img className="d-block w-100 img-carousel-left" src={Banner2} alt="..." />
             </Carousel.Item>
             <Carousel.Item interval={4000}>
-              <img className="d-block w-100" src={Banner3} alt="..." />
+              <img className="d-block w-100 img-carousel-center" src={Banner3} alt="..." />
             </Carousel.Item>
         </Carousel>
         <div>
           <h4 className="text-center">Nuevos emprendimientos</h4>
           <div>
-            <div className="card col-2">
+            <div className="card">
               <div className="card-body">
                 <div className="card-title">
                   Titulo
