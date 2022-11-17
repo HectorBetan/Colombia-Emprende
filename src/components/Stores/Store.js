4c82e631d9156b11fa6e3f5a8697bb6d18e1a8f3
Hector Betancourt <hhbetancourt92@gmail.com>
2022-11-17 15:52:33 -0500

    storesrpducts

--- src/components/Stores/Store.js
+++ src/components/Stores/Store.js
@@ -4,6 +4,9 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { useState, useEffect } from "react";
 import imgStore from "../../assets/img-store.jpg";
+import Tooltip from 'react-bootstrap/Tooltip';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 function Store(data) {
   const [cargando, setCargando] = useState(true);
   const [ant, setAnt] = useState(false);
@@ -78,17 +81,192 @@ function Store(data) {
         document.getElementById(id+"success").classList.add("d-none");
       }, 4000);
     }
+    const PhotoProducts = (foto) =>{
+      let fotos = foto.foto
+      if (fotos){
+        return(
+          <img onClick={(e)=>{
+            e.preventDefault();
+            handleShowModal();
+          }}
+          className="d-block rounded img-producto"
+          src={fotos[0]}
+          alt="img"
+          />
+        )
+      } else{
+
+      }
+    }
+    const formatterPeso = new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+      minimumFractionDigits: 0
+    })
+    const PopOver = (data) =>{
+      let desc = data.data.desc
+      let i =  data.data.i
+      const [show, setShow] = useState(false)
+      console.log(desc)
+      let popover
+              
+                popover = (
+                  <Popover id={i} key={i}>
+                    <Popover.Header as="h3" className="d-flex flex-row justify-content-between">
+                    Descripción
+                    <button type="button" className="btn-close text-end" aria-label="Close" onClick={(e)=>{
+                      e.preventDefault()
+                      setShow(false)
+                    }}></button></Popover.Header>
+                    <Popover.Body>
+                      {desc}
+                    </Popover.Body>
+                  </Popover>
+                );
+              
+      return(
+        <OverlayTrigger show={show} trigger="click" placement="bottom" overlay={popover}>
+                            <span role="button" onClick={(e)=>{
+                              e.preventDefault()
+                              setShow(!show)
+                            }}>Ver más...</span>
+                            </OverlayTrigger>
+      )
+      
+    }
     const ExistsProducts = () => {
       if (emprendimiento.value.products){
         return(
           <div>
-            {emprendimiento.value.products.map((producto) => {
-              return(
-                <div key={producto._id} className="d-flex flex-row">
-                  <h3>{producto.Nombre}</h3>
-                  <input type="number" min="1" max="99" id={producto._id} defaultValue="0"/>
-                  <button onClick={(e) => {e.preventDefault(); agregar(producto) }}>Agregar al carrito</button>
-                  <div className="m-1 d-none" id={`${producto._id}success`} ><Alert /></div>
+            {emprendimiento.value.products.map((producto, i) => {
+              let precio = formatterPeso.format(producto.Precio)
+              let fotos
+              let desc
+              let verMas
+              if(producto.Descripcion.length){
+                if(w<=950){
+                  if (producto.Descripcion.length > 100){
+                    desc = producto.Descripcion.substring(0,98).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                } else if(w<=1000){
+                  if (producto.Descripcion.length > 118){
+                    desc = producto.Descripcion.substring(0,116).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                }else if(w<=1100){
+                  if (producto.Descripcion.length > 130){
+                    desc = producto.Descripcion.substring(0,128).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                } 
+                else if(w<=1200){
+                  if (producto.Descripcion.length > 162){
+                    desc = producto.Descripcion.substring(0,160).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                } 
+                else if(w<=1250){
+                  if (producto.Descripcion.length > 182){
+                    desc = producto.Descripcion.substring(0,180).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                } 
+                else if(w>1300){
+                  if (producto.Descripcion.length > 240){
+                    desc = producto.Descripcion.substring(0,238).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                }else{
+                  if (producto.Descripcion.length > 220){
+                    desc = producto.Descripcion.substring(0,218).concat("... ")
+                    verMas = true
+                  } else {
+                    desc = producto.Descripcion
+                  }
+                }
+                
+              }
+              console.log(producto.Nombre)
+              console.log(producto.Descripcion.length)
+              if (producto.Imagen){
+                fotos = producto.Imagen.split(",")
+              } else {
+                fotos = [imgStore]
+              }
+              let c 
+              if (w> 370){
+                c= "Cantidad:"
+              } else{
+                c="Cant:"
+              }
+              
+              return(<div className="card m-3 p-3">
+                <div key={producto._id} className="d-flex flex-row justify-content-between">
+                  
+                    <div className="d-flex flex-row  justify-content-between">
+                      <div className="d-flex flex-column justify-content-center">
+                        <PhotoProducts foto={fotos} />
+                      </div>
+                      <div className="d-flex flex-column ms-2 me-3 ms-md-3 me-md-2 ps-md-2 pe-md-2 ms-lg-3 me-lg-3 ps-lg-3 pe-lg-3 ms-xl-3 me-xl-3 ps-xl-3 pe-xl-3 ms-xxl-3 me-xxl-3 ps-xxl-3 pe-xxl-3  justify-content-center producto-data">
+                        <h3 className="ps-2 pe-2">{producto.Nombre}</h3>
+                        <h5 className="ps-2 pe-2">Precio: {precio}</h5>
+                        {producto.Descripcion && w>600 && <div className="ps-2 pe-2 desc">
+                          Descripción: <span>{desc}</span>
+                          {verMas && <span className="ver-mas-descripcion">
+                            <PopOver data={{desc:producto.Descripcion, i:i}} />
+                          </span>
+                          }
+                          </div>}
+                      </div>
+                    </div>
+                    {w >= 750 && 
+                    <div  className="d-flex flex-column justify-content-center">
+                      <div className="d-flex flex-row  justify-content-evenly agregar-carrito-input">
+                        <label>Cantidad:</label>
+                        <input type="number" min="1" max="99" id={producto._id} defaultValue="0"/>
+
+                      </div>
+                      <button className="btn btn-primary boton-agregar-carrito" onClick={(e) => {e.preventDefault(); agregar(producto) }}>
+                        Agregar al carrito
+                      </button>
+                      <div className="m-1 d-none" id={`${producto._id}success`} ><Alert /></div>
+                    </div>
+                    }  
+                  
+                </div>
+                {producto.Descripcion && w<=600 && <div className="ps-2 pe-2 desc">
+                          Descripción: <span>{desc}</span>
+                          {verMas && <span className="ver-mas-descripcion">
+                            <PopOver data={{desc:producto.Descripcion, i:i}} />
+                          </span>
+                          }
+                          </div>}
+                {w < 750 && 
+                  <div  className="d-flex flex-row justify-content-center mt-3 stores-btn-a">
+                    <div className="d-flex flex-row  justify-content-evenly agregar-carrito-input-cel">
+                      <label>{c}</label>
+                      <input type="number" min="1" max="99" id={producto._id} defaultValue="0"/>
+
+                    </div>
+                    <button className="btn btn-primary boton-agregar-carrito-cel" onClick={(e) => {e.preventDefault(); agregar(producto) }}>
+                      Agregar al carrito
+                    </button>
+                    <div className="m-1 d-none" id={`${producto._id}success`} ><Alert /></div>
+                  </div>
+                  }  
                 </div>
               )
             })
@@ -298,7 +476,7 @@ function Store(data) {
                       handleShowModal();
         
                     }}
-                    className="d-block  rounded img-principal-cel"
+                    className="d-block  rounded img-principal"
                     src={fotos[0]}
                     alt="img"
                     />
@@ -358,17 +536,17 @@ function Store(data) {
       if (emprendimiento){
         return (
             <div className="card d-flex flex-row justify-content-start">
-              {w >= 550 && <PhotosView />}
+              {w >= 600 && <PhotosView />}
               
               
               
               <div className="card-body  d-flex flex-column justify-content-center caja-datos">
-              {w >= 550 && <h5  className="card-title store-title">{emprendimiento.value.store.Nombre}</h5>}
+              {w >= 600 && <h5  className="card-title store-title">{emprendimiento.value.store.Nombre}</h5>}
                 {emprendimiento.value.store.Calificacion && <div>
-                  {w >= 550 && <CalificacionView />}
+                  {w >= 600 && <CalificacionView />}
                 </div>}
-                {w >= 550 && <h4 className="card-text store-categoria">{w >= 550 && <span>Categoria: </span>}<span className="stores-cel">{emprendimiento.value.store.Categoria}</span></h4>}
-                {w < 550 && <div className="d-flex flex-row mb-2">
+                {w >= 600 && <h4 className="card-text store-categoria">{w >= 600 && <span>Categoria: </span>}<span className="stores-cel">{emprendimiento.value.store.Categoria}</span></h4>}
+                {w < 600 && <div className="d-flex flex-row mb-2">
                   <PhotosViewCel />
                   <div className="d-flex flex-column">
                     <h5 className="card-title store-title store-title-1">{emprendimiento.value.store.Nombre}</h5>
@@ -376,7 +554,7 @@ function Store(data) {
                 <h4 className="card-text store-categoria">{w>350 &&<span>Categoria: </span>}{emprendimiento.value.store.Categoria}</h4>
                   </div>
                   </div>}
-                  {emprendimiento.value.store.Calificacion && w < 550 &&<div className="text-center d-flex flex-row">
+                  {emprendimiento.value.store.Calificacion && w < 600 &&<div className="text-center d-flex flex-row">
                   <span className="me-1">Calificación:</span><CalificacionView />
                 </div>}
                   <h3 className="card-text store-ciudad"><span>Ciudad: </span><span className="stores-cel">{emprendimiento.value.store.Ciudad}</span></h3>
@@ -384,7 +562,7 @@ function Store(data) {
                 <h3 className="card-text store-celular">{w >= 600 && <span>Celular: </span>}{w < 600 && <span>Celular: </span>}<span className="stores-cel">{emprendimiento.value.store.Celular}</span></h3>
                 
                 {emprendimiento.value.store.Telefono &&
-                  w >= 550 &&<h4 className="card-text store-categoria">
+                  w >= 600 &&<h4 className="card-text store-categoria">
                     {w >= 600 && <span>Telefono: </span>}<span className="stores-cel">
                     {emprendimiento.value.store.Telefono}</span>
                   </h4>
