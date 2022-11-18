89e3bb0f281d5c0509e02b388b218200cce783ff
Hector Betancourt <hhbetancourt92@gmail.com>
2022-11-18 15:59:07 -0500

    loadings

--- src/components/HomeAds.js
+++ src/components/HomeAds.js
@@ -9,7 +9,7 @@ function HomeAds() {
     if (loading){
         return(
             <div>
-            <div className="d-flex justify-content-center m-5 p-5">
+            <div className="d-flex justify-content-center m-5">
               <div className="spinner-border" style={{width: "3rem", height: "3rem"}} role="status">
                 <span className="visually-hidden">Loading...</span>
               </div>
